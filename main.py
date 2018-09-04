da9859e4831c9ea05b5758c58d097a0315cd8536
rishikeshdevsot <rishikeshdevsot@gmail.com>
2018-09-04 19:44:02 -0400

    Added comments explaining bothSem and Year

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import const
 
 #The days go from 1 to 7 instead of 0 to 6, for example const.MONDAY is 1 instead of 0
 
-#first argument is the course code+ F/S, (F- Fall, S-winter,  Y-year) 
-#second argument is if it is available in both semesters(or fall or winter)
+#first argument is the course code+ F/S/Y, (F- Fall, S-winter,  Y-year) 
+#second argument is if it is available in both semesters or fall or winter or year
+
+#Difference between both semesters and year is that for both semesters the course will have different timings
+#and you can only choose the course either in fall or winter, it is just offered in both semesters
 myYear = Year()
 
 course1 = course("CSC100F", "Both", const.MONDAY, 9, 30, 10, 0)
